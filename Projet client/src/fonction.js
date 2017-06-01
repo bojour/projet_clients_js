a38310e45b56a7540b72fda0b4976b19c96a0942
bojour <Jonathan.Saurer@rpn.ch>
2017-06-01 14:06:30 +0200

    found better way of deleting, small corrections in the adding function

--- Projet client/src/fonction.js
+++ Projet client/src/fonction.js
@@ -4,15 +4,19 @@ var nom = document.getElementById("txt").value;
 var prenom = document.getElementById("txt1").value;
 var table = document.getElementById("tableToModify");
 var ligne = table.insertRow(-1);
+ligne.id = nom+prenom;
 var colonne1 = ligne.insertCell(0);
 colonne1.innerHTML += nom;
 var colonne2 = ligne.insertCell(1);
 colonne2.innerHTML += prenom;
 var select = document.getElementById("liste");
 var nomC = nom + " " + prenom;
-select.options[select.options.length] = new Option (nomC);    
+select.options[select.options.length] = new Option (nomC);
+
 
 }
+
+
 
 function change()
 {
@@ -25,28 +29,22 @@ document.getElementById("nomV").value = spl[0];
 document.getElementById("prenomV").value = spl[1];
 
 }
+
+
+
+
 function suppr()
 {
-var arrayLignes = document.getElementById("tableToModify").rows;
-var longueur = arrayLignes.length;
-var arrayColonnes;
-var largeur; 
-var confNom = document.getElementById("nomV").value;
+var Nom = document.getElementById("nomV").value;
+var Prenom = document.getElementById("prenomV").value;
 var select = document.getElementById("liste");
-    
-for(var i=0; i<=longueur; i++)
-{
-    arrayColonnes = arrayLignes[i].cells;
-    largeur = arrayColonnes.length;   
-   // alert(arrayColonnes[0].innerHTML + " " + arrayColonnes[1].innerHTML);
-    var n = confNom.includes(arrayColonnes[0].innerHTML)
-    if(n == true)
-        {
-            document.getElementById("tableToModify").deleteRow(i);
-            select.remove(select.selectedIndex);
-            
-        }
-}
+var NomComplet = Nom + Prenom;
+var element = document.getElementById(NomComplet);
 
+    element.parentNode.removeChild(element);
+    
+    
+var x = document.getElementById("liste");
+x.remove(x.selectedIndex);   
 }
 

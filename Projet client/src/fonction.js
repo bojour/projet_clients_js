3f1a0b056c2e7eda2d77934fea85a56563087ee8
bojour <Jonathan.Saurer@rpn.ch>
2017-06-01 11:07:11 +0200

    Add delete function/button

--- Projet client/src/fonction.js
+++ Projet client/src/fonction.js
@@ -9,10 +9,44 @@ colonne1.innerHTML += nom;
 var colonne2 = ligne.insertCell(1);
 colonne2.innerHTML += prenom;
 var select = document.getElementById("liste");
-select.options[select.options.length] = new Option (nom);
+var nomC = nom + " " + prenom;
+select.options[select.options.length] = new Option (nomC);    
+
 }
 
+function change()
+{
+   var select = document.getElementById("liste");
+var choice = select.selectedIndex;
+var valeur = select.options[choice].value;
+var texte = select.options[choice].text;
+var spl = texte.split(" ");
+document.getElementById("nomV").value = spl[0];
+document.getElementById("prenomV").value = spl[1];
+
+}
 function suppr()
 {
+var arrayLignes = document.getElementById("tableToModify").rows;
+var longueur = arrayLignes.length;
+var arrayColonnes;
+var largeur; 
+var confNom = document.getElementById("nomV").value;
+var select = document.getElementById("liste");
     
+for(var i=0; i<=longueur; i++)
+{
+    arrayColonnes = arrayLignes[i].cells;
+    largeur = arrayColonnes.length;   
+   // alert(arrayColonnes[0].innerHTML + " " + arrayColonnes[1].innerHTML);
+    var n = confNom.includes(arrayColonnes[0].innerHTML)
+    if(n == true)
+        {
+            document.getElementById("tableToModify").deleteRow(i);
+            select.remove(select.selectedIndex);
+            
+        }
 }
+
+}
+
